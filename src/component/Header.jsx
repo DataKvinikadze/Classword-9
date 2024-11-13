a375506bc02842afb6b2448f8152227dd9c31381
DataKvinikadze <datakvinikadze07@gmail.com>
2024-11-14 00:24:58 +0400

    feature/create feader structure
    adding header structure

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <div>
+      <div>
+        <h1>Where in the world?</h1>
+        <div>Dark Mode</div>
+      </div>
+    </div>
+  );
 }
